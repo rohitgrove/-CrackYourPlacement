1081cda224835e684e1d43e76702cac812dbcabc
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-07-16 16:31:11 +0530

    RemoveDumplicateNumbers

---
+++ Day1/RemoveDuplicateNumbers.java
@@ -0,0 +1,24 @@
+public class RemoveDuplicateNumbers {
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) { 
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicates(nums1));
+        int nums2[] = { 1, 1, 2 };
+        System.out.println(removeDuplicates(nums2));
+    }
+}
