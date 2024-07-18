844682fa2419cabc890b2e6b2c4f0335857eed88
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-07-18 20:57:33 +0530

    JumpGame

---
+++ Day3/JumpGame.java
@@ -0,0 +1,25 @@
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = { 2, 3, 1, 1, 4 };
+        System.out.println(canJump(nums1));
+
+        int nums2[] = { 3, 2, 1, 0, 4 };
+        System.out.println(canJump(nums2));
+    }
+}
